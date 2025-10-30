8ee693b43148463ff35b019bec43e6549471be70
jmperafan <jmperafan@mail.com>
2025-10-30 09:04:24 +0100

    Fix base path: add trailing slash to config, remove from links

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import sitemap from '@astrojs/sitemap';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jmperafan.github.io',
-  base: '/Juanalytics',
+  base: '/Juanalytics/',
   integrations: [mdx(), sitemap()],
 });
